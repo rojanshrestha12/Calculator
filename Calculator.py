6a0dfc209d1abbcc5a9a82abb8c47e3e4c85979a
rojanshrestha12 <np03cs4s240042@heraldcollege.edu.np>
2025-03-19 12:20:29 +0545

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,7 +1,1 @@
 #make calculator buddy
-a, b = map(float, input("Enter two numbers: ").split())
-
-print("Addition:", add(a, b))
-print("Subtraction:", sub(a, b))
-print("Multiplication:", mul(a, b))
-print("Division:", div(a, b))
