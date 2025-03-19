a53c61451bc219623503cb9dcff40e7b5074fb02
Aman Raut <140369666+aman2072@users.noreply.github.com>
2025-03-19 12:41:58 +0545

    remove division

--- Calculator.py
+++ Calculator.py
@@ -1,10 +1,5 @@
 #make calculator buddy
-def div(a, b):
-    if b == 0:
-        return "Error: Division by zero is not allowed."
-    return a / b
-a, b = map(float, input("Enter two numbers: ").split())
-print("Addition:", add(a, b))
-print("Subtraction:", sub(a, b))
-print("Multiplication:", mul(a, b))
-print("Division:", div(a, b))
+
+#print("Addition:", add(a, b))
+#print("Subtraction:", sub(a, b))
+#print("Multiplication:", mul(a, b))
