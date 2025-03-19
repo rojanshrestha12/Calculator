42ab1aa2a28b77fbc2c08e079d5a9e441f9e3cb4
Rojan Shrestha <rojanstha621@gmail.com>
2025-03-19 12:05:26 +0545

    madechangers

--- Calculator.py
+++ Calculator.py
@@ -1,1 +1,7 @@
 #make calculator buddy
+a, b = map(float, input("Enter two numbers: ").split())
+
+print("Addition:", add(a, b))
+print("Subtraction:", sub(a, b))
+print("Multiplication:", mul(a, b))
+print("Division:", div(a, b))
