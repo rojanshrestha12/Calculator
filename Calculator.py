0904bdcc0b8d335e919b75434eb18467df28b6fc
Aman Raut <140369666+aman2072@users.noreply.github.com>
2025-03-19 12:44:48 +0545

    division operation added in branch

--- Calculator.py
+++ Calculator.py
@@ -3,3 +3,9 @@
 #print("Addition:", add(a, b))
 #print("Subtraction:", sub(a, b))
 #print("Multiplication:", mul(a, b))
+def div(a, b):
+    if b == 0:
+        return "Error: Division by zero is not allowed."
+    return a / b
+a, b = map(float, input("Enter two numbers: ").split())
+print("Division:", div(a, b))
